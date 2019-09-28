beceed866dec213dbb4884e1dd5cc5835784f25e
Alex <msalex54@gmail.com>
2019-09-28 14:34:27 +0300

    First Task

---
+++ src/ID_0002.java
@@ -0,0 +1,36 @@
+//Сумма
+//(Время: 1 сек. Память: 16 Мб Сложность: 19%)
+//Требуется посчитать сумму целых чисел, расположенных между числами 1 и N включительно.
+//
+//Входные данные
+//В единственной строке входного файла INPUT.TXT записано единственное целое число N, не превышающее по абсолютной величине 104.
+//
+//Выходные данные
+//В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел, расположенных между 1 и N включительно.
+
+import java.io.*;
+import java.util.*;
+
+public class ID_0002 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int result = 0;
+        int start = 0;
+        int finish = 0;
+        int a = in.nextInt();
+        if (a >= 1) {
+            start = 1;
+            finish = a;
+        } else{
+            start = a;
+            finish = 1;
+        }
+
+        for (int i = start; i <=finish ; i++) {
+            result+=i;
+        }
+
+        System.out.println(result);
+    }
+}
